d67d5abde0dff1dfdc06df6acf14271996f1a5e3
Maria Simkova <simkova.maria.a@gmail.com>
2022-10-18 19:16:57 +0200

    modify messages

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const ny = dayjs("01.01.2023");
 const msg = document.querySelector(".msg");
 
 if (now.isAfter(ny)) {
-    msg.textContent = "IT'S 2023";
+    msg.textContent = "IT'S 2023! YAY!";
 } else {
-    msg.textContent = "IT'S STILL 2022";
+    msg.textContent = "IT'S STILL 2022...";
 }
